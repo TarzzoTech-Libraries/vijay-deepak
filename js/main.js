7fd84d7f8da49dd127d543c3e24b424c4745f40b
Vijay Deepak <vijaydeepak7195@outlook.com>
2019-03-31 13:55:51 +0530

    updated collapseable header for education

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
   const setCopyRights = function() {
     const copyRights = document.getElementById("copy-rights");
     copyRights.innerHTML = `©Copyright${new Date().getFullYear()}. All rights reserved.`;
-  }
+  };
 
   const setNavActive = function() {
     const navList = document.getElementById("nav-list");
@@ -24,23 +24,41 @@
   const setInitialDivHeight = function() {
     const homeDiv = document.getElementById("home");
     homeDiv.style.height = `${window.screen.availHeight}px`;
-  }
+  };
 
   const setHomeText = function() {
     const homeText = document.getElementById("home-text");
     homeText.innerHTML = `<h2>Hi,</h2><h2>I'm Vijay Deepak</h2>`;
     setInterval(() => {
-      if(homeText.innerHTML === `<h2>Hi,</h2><h2>I'm Vijay Deepak</h2>`) {
+      if (homeText.innerHTML === `<h2>Hi,</h2><h2>I'm Vijay Deepak</h2>`) {
         homeText.innerHTML = `<h2>I am</h2><h2>a Web Developer</h2>`;
       } else {
         homeText.innerHTML = `<h2>Hi,</h2><h2>I'm Vijay Deepak</h2>`;
       }
     }, 3000);
-  }
+  };
+
+  const educationCollapse = function() {
+    const collapseDiv = document.getElementsByClassName(
+      "education-collapsible"
+    );
+    for (let i = 0; i < collapseDiv.length; i++) {
+      collapseDiv[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let content = this.nextElementSibling;
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+    }
+  };
 
   // Call On load
   setCopyRights();
   setNavActive();
   setInitialDivHeight();
   setHomeText();
+  educationCollapse();
 })();
